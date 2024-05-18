0d5c47d2e33d00208d6273ee0676eece37e422e0
sarobidyRAKOT <rakotoarisoasarobidy630@email.com>
2024-05-18 11:07:21 +0300

    sprint1

--- src/mg/itu/Controllers/Controller_2.java
+++ src/mg/itu/Controllers/Controller_2.java
@@ -2,7 +2,10 @@ package mg.itu.Controllers;
 
 import mg.itu.annotation.*;
 
-@Annotation_controller
+@Annotation_controller()
 public class Controller_2 {
     
+    @Get(url = "liste")
+    public void liste_emp () {}
+
 }
